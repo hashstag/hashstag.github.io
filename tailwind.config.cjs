1be69617e4bf64dd198f9c4516f7223b73d55bef
hashstag <service@hashstag.dev>
2025-02-23 22:57:24 +0000

    look: fixed the placeholder page's animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,17 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 }
+        }
+      },
+      animation: {
+        cursor: 'blink 1s step-end infinite'
+      }
+    },
   },
   plugins: [],
 } 
